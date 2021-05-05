352a1c118a6c2408027278bcde866cf80169cd36
Saidur Rahman Setu <srsetu@gmail.com>
2021-05-06 01:35:46 +0600

    CSS Links Added

---
+++ 03-javascript/03_data_types.js
@@ -0,0 +1,19 @@
+// String
+// Number
+// Boolean
+// Array
+// Function
+// Object
+// null
+// undefined
+
+let a; // undefined
+let str = 'Saidur Rahman'; // String
+let num = 45; // Number
+let bool = true; // Boolean
+let arr = ['Saidur', 45, true]; // Object
+let fun = function () {}; // function
+let obj = { a: 45, b: 'Saidur' }; // Object
+let n = null; // null
+
+console.log(typeof n);
---
+++ 03-javascript/2_variable.js
@@ -0,0 +1,12 @@
+let name = 'Setu'; // String
+const PI = 3.1416; // Number
+name = 'JS Bangladesh';
+
+// Naming Convention
+let first_name = 'Saidur';
+let firstName = 'Saidur';
+
+// Rules
+let age45 = 45;
+
+console.log(age45);
