5bcb4ad57886bfbf417c134fe50e782748d58f79
Saidur Rahman Setu <srsetu@gmail.com>
2021-05-15 16:35:59 +0600

    JavaScript Array Added

---
+++ 03-javascript/04_operator.js
@@ -0,0 +1,26 @@
+let num1 = true;
+let num2 = false;
+
+/* 
+	Addition => +
+	Subtraction => -
+	Multiplication => *
+	Division => /
+	Power => **
+	Modulus => %
+*/
+
+/*
+true && true => true
+true && false => false
+true || true => true
+true || false => true
+*/
+
+console.log(!false);
+// => true && true
+// => true && false
+
+let num = true ? 'In case of true' : 'in case of false';
+
+console.log(num);
---
+++ 03-javascript/array.js
@@ -0,0 +1,10 @@
+let person = ['Saidur', 28, true];
+
+// console.log(person.length);
+
+let arr = [
+    ['Saidur', 'Manikganj'],
+    ['JS Bangaldesh', 'Manikganj'],
+];
+
+console.log(arr[0][1]);
